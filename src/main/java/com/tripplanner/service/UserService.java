02bed61a7cbfddf9b673a65a83b93a29580d7c87
Navya S Shetty <154506431+Navya-S-Shetty@users.noreply.github.com>
2026-07-14 09:35:03 +0530

    Remove unnecessary comment in UserService
    Removed comment about method placement.

--- src/main/java/com/tripplanner/service/UserService.java
+++ src/main/java/com/tripplanner/service/UserService.java
@@ -32,7 +32,6 @@ public class UserService implements UserDetailsService {
         );
     }
 
-    // This must be INSIDE the class curly braces!
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
